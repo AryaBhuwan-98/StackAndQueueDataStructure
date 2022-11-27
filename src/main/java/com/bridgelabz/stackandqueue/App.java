7175422c629f26b814a98e4815f40b68db92f716
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2022-11-27 15:32:47 -0800

    Adding Welcome message of stack and queue

---
+++ src/main/java/com/bridgelabz/stackandqueue/App.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.stackandqueue;
+public class App 
+{
+	    public static void main( String[] args )
+	    {
+	        System.out.println( "Welcome To Stack and Queue Data Structure" );
+	    }
+}
